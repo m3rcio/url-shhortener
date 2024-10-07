6677805aa3d91ae12f7661ce204aaa6dafe35d3e
Mércio <mercio20a@gmail.com>
2024-10-06 22:24:34 -0300

    testando o código

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 require('dotenv').config();
 const express = require('express');
-const cors = require('cors');
 const app = express();
-
-// Basic Configuration
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+// app.use(cors());
+app.use(express.json())
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -19,6 +17,31 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+
+const urlDatabase= {};
+let idCounter=1;
+
+const generateShortcode =()=>{
+  return (idCounter++).toString(36);
+}
+app.use(express.json());
+
+
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
+
+app.get('/shorten',(req,res)=>{
+  const {originalUrl}= req.body;
+  if(!originalUrl){
+    return res.status(400).json({error:'url not provided'});
+  }
+
+  const shortCode = generateShortcode();
+  urlDatabase[shortCode] = originalUrl;
+
+  res.json({
+    originalUrl,shortUrl:`http://localhost:${port}/${shortCode}`,
+  })
+})
+
