b144256db069d4e8f215953ab108f2e166605467
Mércio <mercio20a@gmail.com>
2024-10-19 20:47:00 -0300

    teste da api

--- index.js
+++ index.js
@@ -1,47 +1,46 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+// const crypto= require('crypto')
+const mongoose= require('mongoose')
+const ShortUrl= require('./shortUrl')
+const shortid=require('shortid')
 const port = process.env.PORT || 3000;
-
+mongoose.connect('mongodb://127.0.0.1:27017/urlShortener')
 // app.use(cors());
+app.set('view engine', 'ejs');
 app.use(express.json())
+app.use(express.urlencoded({ extended: false }));
+// app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.use('/public', express.static(`${process.cwd()}/public`));
-
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
-});
-
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/', async (req, res) =>{
+  const shortUrls=await ShortUrl.find()
+  res.render('index',{shortUrls:shortUrls})
 });
 
 
-const urlDatabase= {};
-let idCounter=1;
-
-const generateShortcode =()=>{
-  return (idCounter++).toString(36);
-}
 app.use(express.json());
 
 
-app.listen(port, function() {
-  console.log(`Listening on port ${port}`);
+app.post('/shortUrls', async (req, res) => {  
+  const short = shortid.generate(); 
+  await ShortUrl.create({ full: req.body.fullUrl, short: short });
+  // res.redirect('/')
+  res.json({
+    short: short
+  })
 });
 
-app.get('/shorten',(req,res)=>{
-  const {originalUrl}= req.body;
-  if(!originalUrl){
-    return res.status(400).json({error:'url not provided'});
-  }
-
-  const shortCode = generateShortcode();
-  urlDatabase[shortCode] = originalUrl;
-
-  res.json({
-    originalUrl,shortUrl:`http://localhost:${port}/${shortCode}`,
+app.get('/:shortUrl',async (req,res)=>{
+  const shortUrl= await ShortUrl.findOne({
+    short:req.params.shortUrl
   })
+  if(shortUrl==null) return res.sendStatus(404)
+
+  shortUrl.clicks++
+  res.redirect(shortUrl.full)
 })
 
+app.listen(port, () => {
+  console.log(`URL shortener listening at http://localhost:${port}`);
+});
